169b2671ef6bf5aac55177c2927c7e7dbf3f257d
Nivedkp01 <nivedkp001@gmail.com>
2024-10-06 04:51:18 +0530

    RAG

---
+++ server/RAG/blh.py
@@ -0,0 +1,5 @@
+from query_data import qandr
+
+query="what is a nervous system in two sentences?"
+response=qandr(query)
+print(response)
--- server/RAG/compare_embeddings.py
+++ server/RAG/compare_embeddings.py
@@ -6,10 +6,9 @@ import os
 
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
-openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Set OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def main():
     # Get embedding for a word.
