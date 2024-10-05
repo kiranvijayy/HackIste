4d8d0fa85f226d2a5058edb0a77184118f72aec3
Nivedkp01 <nivedkp001@gmail.com>
2024-10-06 01:00:39 +0530

    sign in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Updated import for Routes
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home.jsx";
+import Create from "./pages/Create/Create.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Auth from "./pages/Auth/Auth.jsx";
+import Sign from "./pages/Sign/Sign.jsx";
+import Reg from "./pages/Reg/Reg.jsx";
+
 
 const App = () => {
   return (
     <Router>
       <div className="container justify-center" style={{ margin: "0 auto" }}>
-        <Routes> {/* Updated from Switch to Routes */}
-          <Route path="/" element={<Home />}/>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Self-closing */}
+          <Route path="/login" element={<Login />} /> {/* Self-closing */}
+          <Route path="/create" element={<Create />} />
+
+          <Route path='/auth' element={<Auth />} /> {/* Self-closing */}
+
+          <Route path='/Register' element={<Reg />} />
         </Routes>
       </div>
     </Router>
